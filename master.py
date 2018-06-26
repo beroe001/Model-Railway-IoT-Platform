4b9db58a3a65761b357206dfb936f8a0fa950def
beroe001 <40599036+beroe001@users.noreply.github.com>
2018-06-26 16:02:47 +0200

    Upload of the raspberry pi master python file

---
+++ master.py
@@ -0,0 +1,26 @@
+# Code for communication between I2C port expander and database
+
+# Catching change from database
+
+
+
+
+
+
+
+# Setting change in the physical system
+
+
+
+
+
+
+# Catching change from the physical system
+
+
+
+
+
+
+
+# Store change in the database
